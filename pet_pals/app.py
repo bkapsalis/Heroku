c244d628ff736e8891c2ddcfe49f445e95876dd9
Bill-Kapsalis <bkapsalis@gmail.com>
2019-11-19 17:11:21 -0800

    Added app.config

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -17,8 +17,8 @@ app = Flask(__name__)
 #################################################
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 db = SQLAlchemy(app)
 
 from .models import Pet
